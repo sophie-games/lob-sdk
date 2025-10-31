f07562a8c122c0d5b8a7cda394f36a8816f980f8
Leonardo Massaroli <leonardomassaroli@Leonardos-MacBook-Pro.local>
2025-10-31 17:41:38 -0300

    add face order

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,13 +1,14 @@
 import { EntityId } from ".";
 
 export enum OrderType {
-  NoOrder = 7,
+  NoOrder = 0,
   Walk = 1,
   Run = 2,
   Shoot = 3,
   FireAndAdvance = 4,
   PlaceEntity = 5,
   Fallback = 6,
+  Face = 7,
 }
 
 interface BaseOrder {
@@ -57,6 +58,16 @@ export interface ShootLocationOrder extends BaseOrder {
   pos: [number, number];
 }
 
+export interface FaceTargetOrder extends BaseOrder {
+  type: OrderType.Face;
+  targetId: EntityId;
+}
+
+export interface FaceLocationOrder extends BaseOrder {
+  type: OrderType.Face;
+  pos: [number, number];
+}
+
 export interface FireAndAdvanceToTargetOrder extends BaseOrder {
   type: OrderType.FireAndAdvance;
   targetId: EntityId;
@@ -82,10 +93,13 @@ export type AnyOrder =
   | RunFollowOrder
   | ShootTargetOrder
   | ShootLocationOrder
+  | FaceTargetOrder
+  | FaceLocationOrder
   | FireAndAdvanceToTargetOrder
   | FireAndAdvanceOnPathOrder
   | PlaceEntityOrder
   | FallbackOrder;
+
 export type PathOrderType =
   | OrderType.Walk
   | OrderType.FireAndAdvance
