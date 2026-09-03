ebddef8c0e308d4099c86807b883a9babc309ecd
Leonardo Massaroli <leoflood98@gmail.com>
2026-09-03 14:34:54 -0300

    feat(scenarios): add Twin River Valley

--- src/game-data/eras/napoleonic/scenario-catalog.ts
+++ src/game-data/eras/napoleonic/scenario-catalog.ts
@@ -32,6 +32,7 @@ import leipzig from "./scenarios/leipzig.json";
 import tutorial from "./scenarios/tutorial.json";
 import lineOfBattle from "./scenarios/line-of-battle.json";
 import oneHundredVsOneHundred from "./scenarios/100-vs-100.json";
+import twinRiverValley from "./scenarios/twin-river-valley.json";
 
 export const napoleonicScenarioCatalog = {
   plains,
@@ -67,4 +68,5 @@ export const napoleonicScenarioCatalog = {
   tutorial,
   "line-of-battle": lineOfBattle,
   "100-vs-100": oneHundredVsOneHundred,
+  "twin-river-valley": twinRiverValley,
 } as unknown as ScenarioCatalog;
